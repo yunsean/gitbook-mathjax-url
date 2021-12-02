fcbb98308e9235178ae2a1caaa87ae130d384135
Dylan <yunsean@163.com>
2021-12-02 16:12:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var moment = require('moment');
 module.exports = {
   hooks: {
     'page:before': function(page) {
-      var str = '<script type="text/javascript" src="https://cdn.bootcss.com/mathjax/2.7.3/latest.js?config=TeX-AMS-MML_HTMLorMML"></script>\n<script type="text/x-mathjax-config">\n    MathJax.Hub.Config({\n        tex2jax: {\n            inlineMath: [['$$','$$'], ['$','$'], ['\\(','\\)']],\n            processEscapes: true\n        }\n    });\n</script> '
+      var str = '<script type="text/javascript" src="https://cdn.bootcss.com/mathjax/2.7.3/latest.js?config=TeX-AMS-MML_HTMLorMML"></script>\n<script type="text/x-mathjax-config">\n    MathJax.Hub.Config({\n        tex2jax: {\n            inlineMath: [[\'$\',\'$\']],\n            processEscapes: true\n        }\n    });\n</script> ';
       page.header = page.content + str;
       return page;
     }
