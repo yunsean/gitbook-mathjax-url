3c744c0e26fc67eb511c025c2245d0cb6f958e90
Dylan <yunsean@163.com>
2021-12-02 17:46:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = {
   hooks: {
     'page:before': function(page) {
       var str = '<script type="text/javascript" src="https://cdn.bootcss.com/mathjax/2.7.3/latest.js?config=TeX-AMS-MML_HTMLorMML"></script>\n<script type="text/x-mathjax-config">\n    MathJax.Hub.Config({\n        tex2jax: {\n            inlineMath: [[\'$\',\'$\']],\n            processEscapes: true\n        }\n    });\n</script> ';
-      page.content = page.content + str;
+      page.content = str + page.content;
       return page;
     }
   }
